bc2a6de685d68d5f1a77b21e4cbf13bbdcaa5fdf
cavallaroc9 <cavallaroc9@gmail.com>
2017-09-27 13:32:41 -0400

    Add handler that will allow clicks in each cell
    Handlers will find the element that was clicked and if element is empty

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,9 +4,13 @@ const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const events = require('./game-board/events')
 
+let cell
+
 $(() => {
   setAPIOrigin(location, config)
 })
+
+let test = '#box0'
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
@@ -14,14 +18,21 @@ $(() => {
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+// $(test).on('click', function ()
+
 $(() => {
-  $('#box0').on('click', function () {
-    if ($(this).html().trim()) {
-      console.log('Not empty')
-    } else {
-      console.log('empty')
-      $(this).text('X')
-    }
+  $(document).on('click', function (event) {
+    let cell = ''
+    cell = '#' + event.target.id + ''
+    $(cell).on('click', function () {
+      console.log('this is', this)
+      if ($(this).html().trim()) {
+        console.log('Not empty')
+      } else {
+        console.log('empty')
+        $(this).text('X')
+      }
+    })
   })
   $('#box1').on('click', function () {
     $(this).text('O')
