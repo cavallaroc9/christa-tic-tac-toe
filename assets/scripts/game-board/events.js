022dc6570a0298349e669bbab0a62f54b00a7d1d
cavallaroc9 <cavallaroc9@gmail.com>
2017-09-27 20:02:09 -0400

    Add over variable to end game if there is a winner
    Need to add draw logic

--- assets/scripts/game-board/events.js
+++ assets/scripts/game-board/events.js
@@ -1,25 +1,32 @@
 // Using your knowledge of jQuery write a function, onSubmitForm, that console
 // logs the input in the input field when "save changes" is clicked
-let turn = 'playerX'
+let turn = 'Player X'
+let over = false
+
+const displayWinner = function () {
+  $('#win-or-draw').text(turn + ' WINS!!')
+  over = true
+  return console.log('winner winner chicken dinner!')
+}
 
 // if array contains winning combo, end game and alert winner
 const findWinner = function (cellsArray) {
   if (cellsArray[0] !== '' && cellsArray[0] === cellsArray[1] && cellsArray[1] === cellsArray[2]) {
-    return console.log('winner winner chicken dinner!')
+    displayWinner()
   } else if (cellsArray[3] !== '' && cellsArray[3] === cellsArray[4] && cellsArray[4] === cellsArray[5]) {
-    return console.log('winner winner chicken dinner!')
+    displayWinner()
   } else if (cellsArray[6] !== '' && cellsArray[6] === cellsArray[7] && cellsArray[7] === cellsArray[8]) {
-    return console.log('winner winner chicken dinner!')
+    displayWinner()
   } else if (cellsArray[0] !== '' && cellsArray[0] === cellsArray[3] && cellsArray[3] === cellsArray[6]) {
-    return console.log('winner winner chicken dinner!')
+    displayWinner()
   } else if (cellsArray[1] !== '' && cellsArray[1] === cellsArray[4] && cellsArray[4] === cellsArray[7]) {
-    return console.log('winner winner chicken dinner!')
+    displayWinner()
   } else if (cellsArray[2] !== '' && cellsArray[2] === cellsArray[5] && cellsArray[5] === cellsArray[8]) {
-    return console.log('winner winner chicken dinner!')
+    displayWinner()
   } else if (cellsArray[0] !== '' && cellsArray[0] === cellsArray[4] && cellsArray[4] === cellsArray[8]) {
-    return console.log('winner winner chicken dinner!')
+    displayWinner()
   } else if (cellsArray[2] !== '' && cellsArray[2] === cellsArray[4] && cellsArray[4] === cellsArray[6]) {
-    return console.log('winner winner chicken dinner!')
+    displayWinner()
   }
 }
 
@@ -34,14 +41,14 @@ const hasNoMarker = function (event) {
 }
 
 const markBoard = function (event, cellsArray) {
-  if (turn === 'playerX' && hasNoMarker(event) === true) {
+  if (over === false && turn === 'Player X' && hasNoMarker(event) === true) {
     $(event.target).text('X')
     cellsArray[event.target.id] = 'x'
     console.log(cellsArray)
     findWinner(cellsArray)
     switchTurn()
     console.log('player is', turn)
-  } else if (turn === 'playerO' && hasNoMarker(event) === true) {
+  } else if (over === false && turn === 'Player O' && hasNoMarker(event) === true) {
     $(event.target).text('O')
     cellsArray[event.target.id] = 'o'
     console.log(cellsArray)
@@ -57,12 +64,12 @@ const markBoard = function (event, cellsArray) {
 // Change turn to playerO
 //
 const switchTurn = function () {
-  if (turn === 'playerX') {
-    turn = 'playerO'
+  if (turn === 'Player X') {
+    turn = 'Player O'
     console.log('Player O turn')
   } else {
     console.log('Player X turn')
-    turn = 'playerX'
+    turn = 'Player X'
   }
 }
 
